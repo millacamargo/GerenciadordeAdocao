65c00ab5852d3a7648a127d71d895153801506a5
Milla Camargo <milamx98@hotmail.com>
2018-06-10 02:53:59 +0000

    Atualização de controllers e models para enviar ao repositorio

--- ci/application/views/Save_Animal.php
+++ ci/application/views/Save_Animal.php
@@ -101,7 +101,6 @@
 					        	<td><?php echo $animal['cor'];?></td>
 					        	<td><?php echo $animal['idade'];?></td>
 					        	<td><?php echo $animal['sexo'];?></td>
-					        	<!--<td><button type="submit" name="submit" class="primary-btn" onclick="return confirm('Deseja realmente excluir o animal?');">Remover</button></td>-->
 					      	</tr>
       					<?php } ?>
     					</tbody>
@@ -129,18 +128,5 @@
 			<!-- End footer Area -->	
 
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-			<!--<script src="<?= base_url();?>assets/js/vendor/jquery-2.2.4.min.js"></script>-->
-			<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>-->
-			<!--<script src="<?= base_url();?>assets/js/vendor/bootstrap.min.js"></script>			-->
-			<!--<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>-->
-  	<!--		<script src="<?= base_url();?>assets/js/easing.min.js"></script>			-->
-			<!--<script src="<?= base_url();?>assets/js/hoverIntent.js"></script>-->
-			<!--<script src="<?= base_url();?>assets/js/superfish.min.js"></script>	-->
-			<!--<script src="<?= base_url();?>assets/js/jquery.ajaxchimp.min.js"></script>-->
-			<!--<script src="<?= base_url();?>assets/js/jquery.magnific-popup.min.js"></script>	-->
-			<!--<script src="<?= base_url();?>assets/js/owl.carousel.min.js"></script>						-->
-			<!--<script src="<?= base_url();?>assets/js/jquery.nice-select.min.js"></script>							-->
-			<!--<script src="<?= base_url();?>assets/js/mail-script.js"></script>	-->
-			<!--<script src="<?= base_url();?>assets/js/main.js"></script>	-->
 		</body>
 	</html>
--- ci/application/views/Sucesso.php
+++ ci/application/views/Sucesso.php
@@ -29,10 +29,6 @@
 			<link rel="stylesheet" href="<?= base_url();?>assets/css/owl.carousel.css">
 			<link rel="stylesheet" href="<?= base_url();?>assets/css/main.css">
 			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-			<!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
-			<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>-->
-			<!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>-->
-			<!--<script type="text/javascript" src="<?= base_url();?>assets/js/registerpet.js"></script>-->
 		</head>
 		<body>
 			  <header id="header" id="home">
@@ -103,7 +99,6 @@
 			</footer>
 			<!-- End footer Area -->	
 			
-			<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 			<script src="<?= base_url();?>assets/js/vendor/bootstrap.min.js"></script>	
 			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
