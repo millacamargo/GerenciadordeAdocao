66daeffbd599512352a7d3aba6181c6fb1ae461d
Luiza Vasconcelos <luiza_helena_matos@hotmail.com>
2018-06-12 04:56:01 +0000

    Criação de todos os metodos responsaveis por identificar os dados inseridos e instanciá-los fazendo o tratamento dos dados para conseguir mandar para o banco de dados

--- ci/application/models/Volunteer.php
+++ ci/application/models/Volunteer.php
@@ -1,9 +1,17 @@
 <?php
 
+
 class Volunteer {
     
     private $firstName, $lastName, $enderVol, $cidadeVol, $cepVol, $emailVol, $celVol, $diasDisp, $sobreVol, $id;
-    
+    /*
+	 Descrição: Construtor para os objetos do BD.
+	 Autora: Luiza Vasconcelos
+	 Horas: 5 minutos.
+	 Entrada: Dados dos voluntarios.
+	 Saída: não possui valor de retorno, porque você não pode chamar um construtor para um objeto,
+	 você só usa o construtor no momento da inicialização do objeto. 
+	*/
     public function volunteer ($firstName, $lastName, $enderVol, $cidadeVol, $cepVol, $emailVol, $celVol, $diasDisp, $sobreVol, $id=0){
         $this->firstName = $firstName;
         $this->lastName = $lastName;
@@ -17,7 +25,14 @@ class Volunteer {
         $this->id = $id;
     }
     
-  
+   /*
+	 Descrição: Métodos para pegar as informações do BD através do que foi declarado no construtor.  
+	 Autora: Luiza Vasconcelos
+	 Horas: 5 minutos
+	 Entrada: Pega os atributos privados declarados na classe.
+	 Saída: Retorna os atributos.
+	 */
+	 
     public function getFirstName(){
         return $this->firstName;
     }
@@ -49,6 +64,14 @@ class Volunteer {
         return $this->id;
     }
     
+     /*
+	 Descrição: Método para fazer comparação das informações inseridas e não deixar enviar se as mais importantes forem vazias.
+	 Autora: Luiza Vasconcelos
+	 Horas:5 minutos.
+	 Entrada: Pega o que retornou do método get.
+	 Saída: se for diferente de nulo permite a inserção no BD senão dá erro.
+	 */
+    
     public function isValido(){
         return $this->firstName != ""
         && $this->lastName != ""
@@ -60,6 +83,14 @@ class Volunteer {
         && $this->diasDisp != ""
         && $this->sobreVol != "";
     }
+    
+     /*
+	 Descrição: Método que cria-se um array para receber varias informações de requisição.
+	 Autora: Luiza Vasconcelos
+	 Horas: 5 minutos.
+	 Entrada: Os atributos retornados pelo método get.
+	 Saída: Array desses atributos inseridos.
+	 */
    
     public function toArray(){
         $aux = array();
@@ -75,7 +106,13 @@ class Volunteer {
         $aux["id"] = $this->id;
         return $aux;
     }
-    
+        /*
+	 Descrição: Método que pega o nome da tabela do banco para relacionar com o InsertDAO.
+	 Autora: Luiza Vasconcelos
+	 Horas: 5 minutos
+	 Entrada: Dados do voluntario.
+	 Saída: Inserção na tabela voluntarios.
+	*/
      public function getClassName(){
         return "voluntarios";
     }
