1f4951021dc9aafd0cc1dab1800f7fac825b2c81
Luiza Vasconcelos <luiza_helena_matos@hotmail.com>
2018-06-12 04:58:45 +0000

    Criacao da funcao que responderá pela escolha de cidades pré definidas no banco

--- ci/application/models/Cidade_model.php
+++ ci/application/models/Cidade_model.php
@@ -7,12 +7,26 @@ class Cidade_model extends CI_model {
         parent::__construct();
     }
     
-
+ /*
+	 Descrição: Traz todas as ciadades cadastradas no banco de dados.
+	 Autora: Luiza Vasconcelos
+	 Horas: 10 minutos.
+	 Entrada: Dados da tabela Cidade do BD.
+	 Saída: Retorna os dados para o próximo método poder inserir ao select do html.
+	*/
     public function getAll(){
         return $this->db
         ->order_by('cidade')
         ->get('cidade');
     }
+    
+     /*
+	 Descrição: Monta um select de options com as cidades cadastradas.
+	 Autora: Luiza Vasconcelos
+	 Horas: 10 minutos.
+	 Entrada: Dados do getAll que vem da tabela Cidade do BD.
+	 Saída: Options com o dado cidade do BD para poder aparecer dentro do select do html através do array do controller Volunteer.
+	*/
     
     public function selectCidade(){
         $options = "<option value=''>Selecione a Cidade</option>".PHP_EOL;
